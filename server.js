890380077027b444f1be60335498c385322257a0
sikamloïque <loicsikam272@gmail.com>
2022-02-03 00:28:29 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./dist/tuto1'));
+
+app.get('/*', (req, res) =>{
+    res.sendFile('index.html', {root : 'dist/tuto1/'})
+});
+
+app.listen(process.env.PORT || 8000);
